2ef16d83e29a039f536aed0e579abbcc58756f8a
Kishor <kishora.2204@gmail.com>
2026-05-19 16:44:14 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import { auth } from '@/lib/auth'
-import { redirect } from 'next/navigation'
 import LandingPage from './(auth)/landing/page'
 
 export default async function Home() {
-  const session = await auth()
-
-  if (session) {
-    redirect('/dashboard')
-  }
-
   return <LandingPage />
 }
